4fc1b021b4439c52960690fc61e2311e714b67e9
Anna <harba.anna@gmail.com>
2026-02-13 22:46:26 +0100

    test: remove invalid gradio mounting code from ml model test

--- tests/test_ml_model.py
+++ tests/test_ml_model.py
@@ -1,12 +1,3 @@
-from fastapi.testclient import TestClient
-from app import app
-
 def test_app_exists():
     from app import app
     assert app is not None
-
-
-
-from fastapi import FastAPI
-app = FastAPI()
-app = gr.mount_gradio_app(app, demo, path="/")
